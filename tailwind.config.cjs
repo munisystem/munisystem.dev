b8dc1301fe7df6fd02bdd074c5ae589dbb1233de
Yuichi Saito <munisystem@gmail.com>
2023-01-10 22:58:16 +0900

    Format

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,13 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
       fontFamily: {
-        "inter": ["Inter", "sans-serif"],
+        inter: ["Inter", "sans-serif"],
         "source-code-pro": ["Source Code Pro", "sans-serif"],
-      }
+      },
     },
-	},
-	plugins: [],
-}
+  },
+  plugins: [],
+};
