ab036347104a6a29dc2dc329af82b6dc4b27950b
Yuichi Saito <munisystem@gmail.com>
2023-01-10 22:55:10 +0900

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,21 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+import vercel from "@astrojs/vercel/serverless";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://munisystem.dev",
+  integrations: [tailwind()],
+  markdown: {
+    shikiConfig: {
+      theme: "nord",
+      wrap: true
+    }
+  },
+  output: "server",
+  adapter: vercel()
+});
