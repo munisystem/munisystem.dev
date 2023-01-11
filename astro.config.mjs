9f375f8ced102c5f96b9512f4d32a5b029948b36
Yuichi Saito <munisystem@gmail.com>
2023-01-11 16:08:37 +0900

    Tweak

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,21 @@
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
-import vercel from "@astrojs/vercel/serverless";
+import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://munisystem.dev",
+  site: 'https://munisystem.dev',
   integrations: [tailwind()],
   markdown: {
     shikiConfig: {
-      theme: "nord",
+      theme: 'nord',
       wrap: true,
     },
   },
-  output: "server",
+  output: 'server',
   adapter: vercel(),
-});
+})
