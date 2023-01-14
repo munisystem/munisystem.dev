eb0545a56b98843fdf1318a8d36a7656fcd04fab
Yuichi Saito <munisystem@gmail.com>
2023-01-14 11:19:37 +0900

    Setup Google Analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,9 +7,19 @@ import tailwind from '@astrojs/tailwind'
 import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
+import partytown from '@astrojs/partytown'
+
+// https://astro.build/config
 export default defineConfig({
   site: 'https://munisystem.dev',
-  integrations: [tailwind()],
+  integrations: [
+    tailwind(),
+    partytown({
+      config: {
+        forward: ['dataLayer.push'],
+      },
+    }),
+  ],
   markdown: {
     shikiConfig: {
       theme: 'nord',
