4a1ba6b2e10b6d43f273243c231c93e6b8e86a37
Yuichi Saito <munisystem@gmail.com>
2023-01-25 10:39:49 +0900

    Bump astro version to v2.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://munisystem.dev',
+  site: 'https://munisystem.dev/',
   integrations: [tailwind()],
   markdown: {
     shikiConfig: {
