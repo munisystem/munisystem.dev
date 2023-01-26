7e9d33f9ff6a2ee929adbd4a35e21a9e21d86989
Yuichi Saito <munisystem@gmail.com>
2023-01-26 11:54:17 +0900

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,15 +7,18 @@ import tailwind from '@astrojs/tailwind';
 import vercel from '@astrojs/vercel/static';
 
 // https://astro.build/config
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
 export default defineConfig({
   site: 'https://munisystem.dev/',
-  integrations: [tailwind()],
+  integrations: [tailwind(), sitemap()],
   markdown: {
     shikiConfig: {
       theme: 'nord',
-      wrap: false,
-    },
+      wrap: false
+    }
   },
   output: 'static',
-  adapter: vercel(),
+  adapter: vercel()
 });
