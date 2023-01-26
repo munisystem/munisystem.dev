6ac22eb703ad3ebd2bceb0d4e40db39659a48f6f
Yuichi Saito <munisystem@gmail.com>
2023-01-26 09:29:21 +0900

    Tweak

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,12 +1,14 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
+import type { APIContext } from 'astro';
 
-export async function get(context) {
+export async function get(context: APIContext) {
   const posts = await getCollection('posts');
+  const site = context.site ? context.site.toString() : '';
   return rss({
     title: 'munisystem.dev',
     description: "munisystem's blog",
-    site: context.site,
+    site: site,
     items: posts.map((post) => ({
       link: `/posts/${post.slug}`,
       title: post.data.title,
